98889fdc3cd89efd76acb712d97119de67be4590
unknown <juanbianchi@icloud.com>
2024-01-26 00:37:51 -0300

    deleting dist folder

--- dist/src/router/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const linear_controller_1 = require("../controller/linear.controller");
-exports.router = (0, express_1.Router)();
-exports.router.use('/linear', linear_controller_1.linearRouter);
