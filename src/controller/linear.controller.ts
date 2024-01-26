bbda9ca13c44827e6c65bd5227158748ec755e8b
unknown <juanbianchi@icloud.com>
2024-01-25 23:39:19 -0300

    created get endpoint

--- src/controller/linear.controller.ts
+++ src/controller/linear.controller.ts
@@ -2,6 +2,18 @@ import { Router, Request, Response } from 'express'
 
 export const linearRouter = Router()
 
+linearRouter.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <html>
+      <head><title>Success!</title></head>
+      <body>
+        <h1>You did it!</h1>
+        <img src="https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif" alt="Cool kid doing thumbs up" />
+      </body>
+    </html>
+  `)
+})
+
 linearRouter.post('/', (req: Request, res: Response) => {
   const data = req.body;
 
