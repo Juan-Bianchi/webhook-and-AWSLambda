79672668f2fb9fe778417f5db31da0a1d85d6fc0
Juan <juanbianchi@sirius.com.ar>
2024-02-02 21:56:22 -0300

    created new endpoint and new webhooks to differ events

--- src/controller/linear.controller.ts
+++ src/controller/linear.controller.ts
@@ -17,7 +17,8 @@ linearRouter.get('/', (req: Request, res: Response) => {
   `)
 })
 
-linearRouter.post('/',middlewareAuth, (req: Request, res: Response) => {
+linearRouter.post('/', middlewareAuth.bind(null, process.env.WEBHOOK_SECRET), (req: Request, res: Response) => {
+  console.log("\n\n\nIssue webhook\n\n") 
   const payload = req.body;
   const { action, data, type, createdAt } = payload;
   console.log(payload);
@@ -25,6 +26,51 @@ linearRouter.post('/',middlewareAuth, (req: Request, res: Response) => {
   if(!sentToDiscord) {
     res.status(403)
   }
+
+  res.status(200)
+})
+
+linearRouter.post('/cycle', middlewareAuth.bind(null, process.env.CYCLE_SECRET), (req: Request, res: Response) => {
+  console.log("\n\n\nCycle webhook\n\n") 
+  const payload = req.body;
+  console.log(req.headers)
+  console.log(payload);
+
+  res.status(200)
+})
+
+linearRouter.post('/project', middlewareAuth.bind(null, process.env.PROJECT_SECRET), (req: Request, res: Response) => {
+  console.log("\n\n\nProject webhook\n\n") 
+  const payload = req.body;
+  console.log(req.headers)
+  console.log(payload);
+
+  res.status(200)
+})
+
+linearRouter.post('/comment', middlewareAuth.bind(null, process.env.COMMENT_SECRET), (req: Request, res: Response) => {
+  console.log("\n\n\nComment webhook\n\n") 
+  const payload = req.body;
+  console.log(req.headers)
+  console.log(payload);
+
+  res.status(200)
+})
+
+linearRouter.post('/label', middlewareAuth.bind(null, process.env.LABEL_SECRET), (req: Request, res: Response) => {
+  console.log("\n\n\nLabel webhook\n\n") 
+  const payload = req.body;
+  console.log(req.headers)
+  console.log(payload);
+
+  res.status(200)
+})
+
+linearRouter.post('/projectUpdate', middlewareAuth.bind(null, process.env.PROJECT_UPDATE), (req: Request, res: Response) => {
+  console.log("\n\n\nProject update webhook\n\n") 
+  const payload = req.body;
+  console.log(req.headers)
+  console.log(payload);
 
   res.status(200)
 })
--- src/utils/middlewareAuth.ts
+++ src/utils/middlewareAuth.ts
@@ -3,13 +3,13 @@ import crypto from 'crypto';
 import { CustomRequest } from "..";
 
 
-export const middlewareAuth = (req: CustomRequest, res: Response, next: () => any): void => {
+export const middlewareAuth = (secret: string | undefined, req: CustomRequest, res: Response, next: () => any): void => {
   if(req.rawBody === undefined){
     res.sendStatus(400);
   }
   else {
     // Verify signature
-    const signature = crypto.createHmac("sha256", process.env.WEBHOOK_SECRET as string).update(req.rawBody).digest("hex");
+    const signature = crypto.createHmac("sha256", secret as string).update(req.rawBody).digest("hex");
     if (signature !== req.headers['linear-signature']) {
       res.sendStatus(400);
     }
